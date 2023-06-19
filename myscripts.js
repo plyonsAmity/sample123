85f8c2d2449cd70dfd830370698b732d554f33da
plyonsAmity <137048376+plyonsAmity@users.noreply.github.com>
2023-06-19 12:41:22 +0400

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,11 @@
+function goToPage(page){
+	if(page.toLowerCase() == 'oak'){
+		window.location.href = 'oak.html'
+	}
+	if(page.toLowerCase() == 'willow'){
+		window.location.href = 'willow.html'
+	}
+	if(page.toLowerCase() == 'coconut'){
+		window.location.href = 'coconut.html'
+	}
+}
